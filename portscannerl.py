dbf355e2a3595376c68a66194c97f23589676ed6
abhimanyuGupta <91192965+Hackerjedi666@users.noreply.github.com>
2023-09-07 15:00:47 +0530

    Update portscannerl.py

--- portscannerl.py
+++ portscannerl.py
@@ -1,36 +1,44 @@
-
 import sys
 import socket
+import multiprocessing
 
-ip = "<your ip>" 
-open_ports =[] 
+# Function to probe a single port
+def probe_port(ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
+        r = sock.connect_ex((ip, port))
+        sock.close()
+        return port if r == 0 else None
+    except Exception as e:
+        return None
 
-ports = range(1, 10000)
+# Function to probe a single port using a wrapper function
+def probe_port_wrapper(args):
+    ip, port = args
+    return probe_port(ip, port)
 
+# Function to scan ports and detect services using multiprocessing
+def scan_ports(ip, ports, num_threads):
+    pool = multiprocessing.Pool(num_threads)
+    open_ports = list(filter(None, pool.map(probe_port_wrapper, [(ip, port) for port in ports])))
+    return open_ports
 
-def probe_port(ip, port, result = 1): 
-  try: 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    sock.settimeout(0.5) 
-    r = sock.connect_ex((ip, port))   
-    if r == 0: 
-      result = r 
-    sock.close() 
-  except Exception as e: 
-    pass 
-  return result
+def main():
+    ip = input("Enter the target IP: ")
+    ports = range(1, 10000)  # You can specify a different port range
+    num_threads = int(input("Enter the number of threads to use (e.g., 10): "))
 
+    try:
+        open_ports = scan_ports(ip, ports, num_threads)
+        if open_ports:
+            print("Open Ports:")
+            for port in open_ports:
+                print(port)
+        else:
+            print("Looks like no ports are open :(")
+    except KeyboardInterrupt:
+        print("\nScan canceled by user.")
 
-for port in ports: 
-    sys.stdout.flush() 
-    response = probe_port(ip, port) 
-    if response == 0: 
-        open_ports.append(port) 
-    
-
-if open_ports: 
-  print ("Open Ports are: ") 
-  print (sorted(open_ports)) 
-else: 
-  print ("Looks like no ports are open :(")
-
+if __name__ == "__main__":
+    main()
